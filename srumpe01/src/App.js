aade84bec6b9415bf57b88b5f85286296340fdeb
Yeison-Gualteros <yeisongual123@gmail.com>
2024-03-04 20:56:55 -0500

    se agregaron el estudiantecontext.js

--- srumpe01/src/App.js
+++ srumpe01/src/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
 import './App.css';
 import RouterPage from './pages/RouterPage';
-
-
-
-
+import { EstudiantesProvider } from './pages/EstudianteContext';
 
 function App() {
   return (
-    <RouterPage/>
+    <EstudiantesProvider>  {/* Envuelve tu aplicación con el proveedor del contexto */}
+      <RouterPage />
+    </EstudiantesProvider>
   );
 }
 
